4d80c45e71b966e7b0e8e95bcd5ad7e808b2866c
Igor Berdnikov <igor.berdnikov@sofoil.com>
2024-07-28 12:01:50 +0300

    first commit

---
+++ pkg/reader/consoleReader.go
@@ -0,0 +1,32 @@
+package consoleReader
+
+import (
+	"fmt"
+	"strings"
+	"os"
+	"bufio"
+	"strconv"
+)
+
+func ReadString(message string) (string) {
+	fmt.Print(message)
+	var reader = bufio.NewReader(os.Stdin)
+	var input, err = reader.ReadString('\n')
+	if err != nil {
+		fmt.Print("oops! Somthing get wrong!")
+		return ""
+	}
+	return input
+}
+
+
+func ReadNumber(message string) (float64) {
+	var input = ReadString(message)
+	input = strings.TrimSpace(input)
+	var num, err = strconv.ParseFloat(input, 64)
+	if err != nil {
+		fmt.Print("oops! Somthing get wrong!")
+		return -1
+	}
+	return num
+}
